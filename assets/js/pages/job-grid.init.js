856f2d743d38e665f3aafe4622712c35031e4603
akash0234 <81811789+amitavayourflow@users.noreply.github.com>
2022-10-21 20:09:03 +0530

    Done

--- assets/js/pages/job-grid.init.js
+++ assets/js/pages/job-grid.init.js
@@ -1,1 +1,71 @@
-var textRemove=new Choices(document.getElementById("choices-text-remove-button"),{delimiter:",",editItems:!0,maxItemCount:5,removeItemButton:!0});
+<ul class="list-items">
+	<li class="mb-0">
+		<a href="employer-profile.html" class="d-block">
+			<i class="la la-user"></i> My Proile
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="employer-applied-jobs.html" class="d-block">
+			<i class="la la-bookmark"></i> Applied Jobs
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="employer-edit-profile.html" class="d-block">
+			<i class="la la-plus"></i> Add a Resume
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="../contact-us.html" class="d-block">
+			<i class="la la-question"></i> Help
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="employer-edit-profile.html" class="d-block">
+			<i class="la la-gear"></i> Setting
+		</a>
+	</li>
+	<li class="mb-0">
+		<div class="section-block mt-2 mb-2"></div>
+	</li>
+	<li class="mb-0">
+		<a href="../index.html" class="d-block">
+			<i class="la la-power-off"></i> Logout
+		</a>
+	</li>
+</ul>
+
+<ul class="list-items">
+	<li class="mb-0">
+		<a href="employer-profile.html" class="d-block">
+			<i class="la la-user"></i> Company Proile
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="employer-applied-jobs.html" class="d-block">
+			<i class="la la-bookmark"></i> Applied Candidates
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="employer-edit-profile.html" class="d-block">
+			<i class="la la-plus"></i> Add a Resume
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="../contact-us.html" class="d-block">
+			<i class="la la-question"></i> Help
+		</a>
+	</li>
+	<li class="mb-0">
+		<a href="employer-edit-profile.html" class="d-block">
+			<i class="la la-gear"></i>Profile Setting
+		</a>
+	</li>
+	<li class="mb-0">
+		<div class="section-block mt-2 mb-2"></div>
+	</li>
+	<li class="mb-0">
+		<a href="../index.html" class="d-block">
+			<i class="la la-power-off"></i> Logout
+		</a>
+	</li>
+</ul>
